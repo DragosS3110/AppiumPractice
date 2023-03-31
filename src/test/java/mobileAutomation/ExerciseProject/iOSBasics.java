0384b1dad6ab8c446285b3388a8aaaaac149f17f
Dragos <dragos.somlea@wolfpack-digital.com>
2023-03-31 11:30:07 +0300

    Steppers limits check

--- src/test/java/mobileAutomation/ExerciseProject/iOSBasics.java
+++ src/test/java/mobileAutomation/ExerciseProject/iOSBasics.java
@@ -1,6 +1,9 @@
 package mobileAutomation.ExerciseProject;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
 
 import io.appium.java_client.AppiumBy;
 
@@ -11,6 +14,24 @@ public class iOSBasics extends UtilsiOS{
 	{
 
 		
-		driver.findElement(AppiumBy.accessibilityId("Buttons")).click();
+		driver.findElement(AppiumBy.accessibilityId("Steppers")).click();
+		
+		WebElement result = (WebElement) driver.findElement(By.xpath("(//XCUIElementTypeStaticText[@name='0'])[1]"));
+		WebElement defaultMinus = (WebElement) driver.findElement(By.xpath("(//XCUIElementTypeButton[@name='Decrement'])[1]"));
+		WebElement defaultPlus = (WebElement) driver.findElement(By.xpath("(//XCUIElementTypeButton[@name='Increment'])[1]"));
+
+		Assert.assertEquals(result.getAttribute("name"), "0");
+		Assert.assertEquals(defaultMinus.getAttribute("enabled"),"false");
+
+		for(int i=1;i<=10;i++) {
+			
+		defaultPlus.click();
+		if(i==10) {
+		Assert.assertEquals(result.getAttribute("name"), "10");
+		Assert.assertEquals(defaultPlus.getAttribute("enabled"),"false");
+
+		}
+		}
+
 }
 }
