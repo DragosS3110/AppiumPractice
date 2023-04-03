c7bc40b0f06f08b9fdee231062fa262575d01d1a
Dragos <dragos.somlea@wolfpack-digital.com>
2023-04-03 12:33:53 +0300

    Clipboard paste and pressing keys tests

---
+++ src/test/java/mobileAutomation/ExerciseProject/Clipboard.java
@@ -0,0 +1,52 @@
+package mobileAutomation.ExerciseProject;
+
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.DeviceRotation;
+
+import com.google.common.collect.ImmutableMap;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
+
+public class Clipboard extends Utils{
+
+	@Test(priority=1)
+	public void AccessPreferences() throws InterruptedException
+	{
+
+		
+		driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+		
+		driver.findElement(AppiumBy.accessibilityId("3. Preference dependencies")).click();
+		
+		WebElement wifiSettings = (WebElement)driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]"));
+		
+		Assert.assertEquals(wifiSettings.getAttribute("enabled"), "false");
+		
+		driver.findElement(AppiumBy.id("android:id/checkbox")).click();
+		
+		Assert.assertEquals(wifiSettings.getAttribute("enabled"), "true");
+		
+		wifiSettings.click();
+		
+		
+		//Clipboard
+		driver.setClipboardText("Hipsdontlie");
+		driver.findElement(By.id("android:id/edit")).sendKeys(driver.getClipboardText());
+
+		//Pressing keys
+		
+		//driver.pressKey(new KeyEvent(AndroidKey.CAMERA));
+		driver.pressKey(new KeyEvent(AndroidKey.ENTER));
+
+		
+		
+	}
+}
